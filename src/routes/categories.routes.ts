7c65d8cfb4d71ec3162ed728b0fbc9de98542820
csttn <csttn.dev@gmail.com>
2021-03-25 19:15:25 -0300

    add multer and implementation route to upload of category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { deleteCategoryController } from '../modules/cars/useCases/deleteCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 //criando categoria
 categoriesRoutes.post('/', (request, response) => {
@@ -19,6 +26,10 @@ categoriesRoutes.get('/', (request, response) => {
 //deletando categoria por name
 categoriesRoutes.delete('/', (request, response) => {
   return deleteCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
