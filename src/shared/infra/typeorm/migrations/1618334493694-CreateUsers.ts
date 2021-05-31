4ede7b528fc5689df03cbfd53556b91f26a19ff3
csttn <csttn.dev@gmail.com>
2021-05-31 20:05:41 -0300

    fix: ajusted id with primary key in table users

--- src/shared/infra/typeorm/migrations/1618334493694-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1618334493694-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1618334493694 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
