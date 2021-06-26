e987789e77cfe6fc23bf7e8c39e7c20db8a5c7e8
csttn <csttn.dev@gmail.com>
2021-06-25 22:38:47 -0300

    feat: add configuration with coverage tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ import { compilerOptions } from './tsconfig.json';
 export default {
   bail: true,
   clearMocks: true,
+  //coletando informações de cobertura de testes
+  collectCoverage: true,
+  //informando os diretorios para obter as informações de cobertura
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageDirectory: 'coverage',
+  // adicionanmdo opção para gerar summario
+  coverageReporters: ['lcov', 'text-summary'],
   coverageProvider: 'v8',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src/',
