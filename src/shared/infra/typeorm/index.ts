798c02e9017e9f853aef0835d35661cc6a96f4ae
Cleyton Santana <csttn.dev@gmail.com>
2021-10-21 23:17:04 -0300

    fix: correção para executar api fora do container docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,17 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+
+// foi removido o host pois não irie executar as duas aplicações no docker 
+// somente o banco de dados
+
+// export default async (host = 'database'): Promise<Connection> => {
+
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      // host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentalx_test'
